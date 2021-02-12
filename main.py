c18ce12061a1d91024b96a6ac59998d5662e47f3
Maruitimoorea <78821347+Maruitimoorea@users.noreply.github.com>
2021-02-12 10:21:04 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-from chiffrement import*
-from dechiffrement import*
+from chiffrement import chiffrement
+from dechiffrement import dechiffrement
 
-while True:
-    choix = input ("Souhaitez-vous [c]hiffrer ou [d]hiffrer un message ? ")
-    if choix == "c":
-        msg = input("Entrez le message à chiffrer (en lettres capitales, sans espace ni ponctuation) : ")
-        msg_chiffre = chiffrement(msg)
-        print(f"Le message chiffré est {msg_chiffre}.")
-        break
-    elif choix == "d" :
-        msg = input("Entrez le message à déchiffrer (en lettres capitales, sans espace ni ponctuation) : ")
-        msg_clair = dechiffrement(msg)
-        print(f"Le message en clair est {msg_clair}.")
-        break
-    else:
-        print("Recommencez s'il vous plaît.")
+choix = input("souhaitez-vous [c]hiffrer ou [d]échiffrer un message ?")
+if choix == "c":
+    msg = input("entrez le message à chiffrer (en lettres capitales, sans espaces ni plnctuations): ")
+    msg_chiffre = chiffrement(msg)
+    prent(f"Le message chiffré est {msg_chiffre}.")
+elif choix == "d":
+    msg = input("entrez le message à dechiffrer (en lettre capitales, sans espace ni ponctuations) : ")
+    msg_clair = dechiffrement(msg)
+    print(f"Le message en clair est {msg_clair}.")
