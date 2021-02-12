dd95ddd9367012ee65d2d2f228aca2da6cb14f8a
Maruitimoorea <78821347+Maruitimoorea@users.noreply.github.com>
2021-02-12 10:37:13 -1000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ choix = input("souhaitez-vous [c]hiffrer ou [d]échiffrer un message ?")
 if choix == "c":
     msg = input("entrez le message à chiffrer (en lettres capitales, sans espaces ni plnctuations): ")
     msg_chiffre = chiffrement(msg)
-    prent(f"Le message chiffré est {msg_chiffre}.")
+    print(f"Le message chiffré est {msg_chiffre}.")
 elif choix == "d":
     msg = input("entrez le message à dechiffrer (en lettre capitales, sans espace ni ponctuations) : ")
     msg_clair = dechiffrement(msg)
