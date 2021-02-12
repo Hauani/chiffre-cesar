80f5e122dea7de74bc24dd4d8b6d760306917555
Maruitimoorea <78821347+Maruitimoorea@users.noreply.github.com>
2021-02-12 10:32:19 -1000

    Update chiffrement.py

--- chiffrement.py
+++ chiffrement.py
@@ -1,2 +1,9 @@
-def dechiffrement(message_chiffre):
+def chiffrement(message):
+    message_chiffre = ""
+    for lettre in message :
+        nb_lettre = ord(lettre)-65
+        nb_lettre_chiffree = (nb_lettre + 3) % 26
+        lettre_chiffree = chr(nb_lettre_chiffree + 65)
+        message_chiffre += lettre_chiffree
+    return message_chiffre
     
