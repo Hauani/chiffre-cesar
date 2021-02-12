c1bf6b1e63fc20183bbaa7a49856e36fa14d9f05
Maruitimoorea <78821347+Maruitimoorea@users.noreply.github.com>
2021-02-12 10:53:48 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from chiffrement import chiffrement
-from dechiffrement import dechiffrement
+n'imorte quoi 
 
-choix = input("souhaitez-vous [c]hiffrer ou [d]échiffrer un message ?")
-if choix == "c":
-    msg = input("entrez le message à chiffrer (en lettres capitales, sans espaces ni plnctuations): ")
-    msg_chiffre = chiffrement(msg)
-    print(f"Le message chiffré est {msg_chiffre}.")
-elif choix == "d":
-    msg = input("entrez le message à dechiffrer (en lettre capitales, sans espace ni ponctuations) : ")
-    msg_clair = dechiffrement(msg)
-    print(f"Le message en clair est {msg_clair}.")
+n'imorte quoi 
+
+
+n'imorte quoi 
+n'imorte quoi 
+n'imorte quoi 
+n'imorte quoi 
+n'imorte quoi 
+
+
+n'imorte quoi 
+
